f2bf4728fde42ed3fbb5a4ccaacebc3459d1d08e
Aliaa Ahmed ElZohairy <aliaaahmed129@gmail.com>
2017-10-21 07:57:39 +0200

    Add files via upload
    news website

---
+++ task-design/main.js
@@ -0,0 +1,14 @@
+/*navbar link hover show image*/
+
+$(document).ready(function(){
+    $(".hs3").mouseenter(function(){
+        $("#hideshow3").css("display", "block")
+});
+});
+
+$(document).ready(function(){
+           $(".hs3").mouseleave(function(){
+        $("#hideshow3").css("display", "none")
+});
+});
+/*finish navbar link hover show image*/
